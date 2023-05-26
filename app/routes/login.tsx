7bea6db3cf74cf039756942c56f37338b1cf5cf7
Piyush Shrivastava <piyush.shrivastava@mindtickle.com>
2023-05-26 15:27:56 +0530

    initial form functionality in login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,14 +1,113 @@
 import type { LinksFunction } from "@remix-run/node";
-import { Link, useSearchParams } from "@remix-run/react";
+import { Link, useActionData, useSearchParams } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+import { badRequest } from "~/utils/request.server";
 
 import stylesUrl from "~/styles/login.css";
+import type { ActionArgs } from "@remix-run/node";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesUrl },
 ];
 
+function validateUserName(username: string) {
+  if (username.length < 3) {
+    return "Username must be longer than 3 characters";
+  }
+}
+
+function validatePassword(password: string) {
+  if (password.length < 6) {
+    return "Password must be at least 6 digit long";
+  }
+}
+
+function validateUrl(url: string) {
+  const urls = ["/jokes", "/"];
+  if (urls.includes(url)) {
+    return url;
+  }
+  return "/jokes";
+}
+
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData();
+  const loginType = form.get("loginType");
+  const password = form.get("password");
+  const username = form.get("username");
+  const redirectTo = validateUrl(
+    (form.get("redirectTo") as string) || "/jokes"
+  );
+  if (
+    typeof loginType !== "string" ||
+    typeof password !== "string" ||
+    typeof username !== "string"
+  ) {
+    return badRequest({
+      fieldErrors: null,
+      fields: null,
+      formError: "Form not submitted correctly.",
+    });
+  }
+
+  const fields = { loginType, password, username };
+
+  const fieldErrors = {
+    username: validateUserName(username),
+    password: validatePassword(password),
+  };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({
+      fieldErrors,
+      fields,
+      formError: null,
+    });
+  }
+  switch (loginType) {
+    case "login": {
+      // login to get the user
+      // if there's no user, return the fields and a formError
+      // if there is a user, create their session and redirect to /jokes
+      return badRequest({
+        fieldErrors: null,
+        fields,
+        formError: "Not implemented",
+      });
+    }
+    case "register": {
+      const userExists = await db.user.findFirst({
+        where: { username },
+      });
+      if (userExists) {
+        return badRequest({
+          fieldErrors: null,
+          fields,
+          formError: `User with username ${username} already exists`,
+        });
+      }
+      // create the user
+      // create their session and redirect to /jokes
+      return badRequest({
+        fieldErrors: null,
+        fields,
+        formError: "Not implemented",
+      });
+    }
+    default: {
+      return badRequest({
+        fieldErrors: null,
+        fields,
+        formError: "Login type invalid",
+      });
+    }
+  }
+};
+
 export default function Login() {
+  const actionData = useActionData<typeof action>();
   const [searchParams] = useSearchParams();
+
   return (
     <div className="container">
       <div className="content" data-light="">
@@ -26,22 +125,67 @@ export default function Login() {
                 type="radio"
                 name="loginType"
                 value="login"
-                defaultChecked
+                defaultChecked={
+                  !actionData?.fields?.loginType ||
+                  actionData.fields.loginType === "login"
+                }
               />{" "}
               Login
             </label>
             <label>
-              <input type="radio" name="loginType" value="register" /> Register
+              <input
+                type="radio"
+                name="loginType"
+                value="register"
+                defaultChecked={actionData?.fields?.loginType === "register"}
+              />{" "}
+              Register
             </label>
           </fieldset>
           <div>
             <label htmlFor="username-input">Username</label>
-            <input type="text" id="username-input" name="username" />
+            <input
+              type="text"
+              id="username-input"
+              name="username"
+              defaultValue={actionData?.fields?.username}
+            />
+            {actionData?.fieldErrors?.username ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+                id="username-error"
+              >
+                {actionData.fieldErrors.username}
+              </p>
+            ) : null}
           </div>
           <div>
             <label htmlFor="password-input">Password</label>
-            <input id="password-input" name="password" type="password" />
+            <input
+              id="password-input"
+              name="password"
+              type="password"
+              defaultValue={actionData?.fields?.password}
+            />
+            {actionData?.fieldErrors?.password ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+                id="password-error"
+              >
+                {actionData.fieldErrors.password}
+              </p>
+            ) : null}
           </div>
+          <div id="form-error-message">
+            {actionData?.formError ? (
+              <p className="form-validation-error" role="alert">
+                {actionData.formError}
+              </p>
+            ) : null}
+          </div>
+
           <button type="submit" className="button">
             Submit
           </button>
