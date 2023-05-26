1dde94d077ba6520ee79603bc4e6cf3f5f4a9424
Piyush Shrivastava <piyush.shrivastava@mindtickle.com>
2023-05-26 12:41:26 +0530

    added user model in schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -38,8 +38,21 @@ function getJokes() {
 }
 
 async function main() {
+  const kody = await db.user.create({
+    data: {
+      username: "kody",
+      // this is a hashed version of "twixrox"
+      passwordHash:
+        "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
+    },
+  });
   const jokes = getJokes();
-  await Promise.all(jokes.map((joke) => db.joke.create({ data: joke })));
+  await Promise.all(
+    jokes.map((joke) => {
+      const data = { jokesterId: kody.id, ...joke };
+      return db.joke.create({ data });
+    })
+  );
 }
 
 main();
